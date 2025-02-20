959c868bd2c2dfe239ac05ecfe173e8a0b29bc33
AdriAguilar <adritrivi41@gmail.com>
2025-02-20 09:49:50 +0100

    Admin navigation

--- app/src/main/java/com/example/bocatas2/ui/fragments/AdminFragment.kt
+++ app/src/main/java/com/example/bocatas2/ui/fragments/AdminFragment.kt
@@ -5,56 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
 import com.example.bocatas2.R
+import com.example.bocatas2.databinding.FragmentAdminBinding
+import com.example.bocatas2.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AdminFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AdminFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentAdminBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin, container, false)
+        _binding = FragmentAdminBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AdminFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AdminFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+
+        val navController = childFragmentManager.findFragmentById(binding.navHostAdmin.id)
+            ?.let { it as NavHostFragment }?.navController
+
+        binding.logoutBtn.setOnClickListener {
+            auth.signOut()
+            requireActivity().finish()
+            startActivity(requireActivity().intent)
+        }
+
+        binding.bocatasBtn.setOnClickListener { navController?.navigate(R.id.bocatasCrudFragment) }
+        binding.usersBtn.setOnClickListener { navController?.navigate(R.id.usersCrudFragment) }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
